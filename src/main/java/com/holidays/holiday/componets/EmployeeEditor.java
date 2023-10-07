c2610213400040a3581b92d415dac413307f8f8d
Danik <elik@example.com>
2023-10-07 20:41:11 +0300

    all prog, but with errors in constructor

--- src/main/java/com/holidays/holiday/componets/EmployeeEditor.java
+++ src/main/java/com/holidays/holiday/componets/EmployeeEditor.java
@@ -1,21 +1,28 @@
-package com.holidays.holiday.componets;
+package com.holidays.holiday.componets; //файл где не основные элементы страницы
 
 
 import com.holidays.holiday.domein.Employee;
 import com.holidays.holiday.repository.EmployeeRepository;
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.KeyNotifier;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.swing.plaf.basic.BasicMenuUI;
 import java.awt.*;
 
 @SpringComponent
 @UIScope
-public class EmployeeEditor extends VerticalLayout implements KeyNotifier {
+public class EmployeeEditor extends VerticalLayout implements KeyNotifier { //KeyNotifier помогает отслеживать нажатия клавиш
     private final EmployeeRepository employeeRepository;
 
     private Employee employee; //поле содержит сотрудника, которого редактируем
@@ -26,20 +33,66 @@ public class EmployeeEditor extends VerticalLayout implements KeyNotifier {
     TextField lastName = new TextField("Last name");
     TextField patronymic = new TextField("patronymic");
 
-
-
     private Button save = new Button("Save", VaadinIcon.CHECK.create());
-    Button cancel = new Button("Cancel");
-    Button delete = new Button("Delete", VaadinIcon.TRASH.create());
-    HorizontalLayout actions = new HorizontalLayout(save, cancel, delete);
+    private Button cancel = new Button("Cancel");
+    private Button delete = new Button("Delete", VaadinIcon.TRASH.create());
+    private HorizontalLayout actions = new HorizontalLayout(save, cancel, delete);
 
+    private Binder<Employee> binder = new Binder<>(Employee.class);
+    @Setter
+    public ChangeHandler changeHandler;
 
-
-
+    public interface ChangeHandler{
+        void onChange();
+    }
 
     @Autowired
     public EmployeeEditor(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
+
+        add(lastName, firstName, patronymic, actions);
+        binder.bindInstanceFields(this);
+          //внешний вид кнопок
+        setSpacing(true);
+
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+
+        addKeyPressListener(Key.ENTER, e -> save());
+
+
+        save.addClickListener(e -> save());
+        //обработчики нажатий кнопок
+        delete.addClickListener(e -> delete());
+        cancel.addClickListener(e -> editEmployee(employee));
+        setVisible(false);
+    }
+    //удаляем данные
+    private void delete() {
+        employeeRepository.delete(employee);
+        changeHandler.onChange();
+    }
+
+    //сохраняем данные
+    private void save() {
+        employeeRepository.save(employee);
+        changeHandler.onChange();
+    }
+    public void editEmployee(Employee newEmp){
+        if (newEmp == null){ //проверка, если нам вводят ничего
+            setVisible(false); //выводит ничего
+            return;
+        }
+        //ввод айдишника и его проверка
+        if (newEmp.getId() != null){
+           employee = employeeRepository.findById(newEmp.getId()).orElse(newEmp);
+        } else{
+            employee = newEmp;
+        }
+        binder.setBean(employee);
+        //чтобы было видно форму
+        setVisible(true);
+        lastName.focus();
     }
 }
 
--- src/main/java/com/holidays/holiday/view/MainView.java
+++ src/main/java/com/holidays/holiday/view/MainView.java
@@ -1,9 +1,15 @@
 package com.holidays.holiday.view;
 
+
+import com.holidays.holiday.componets.EmployeeEditor;
 import com.holidays.holiday.domein.Employee;
 import com.holidays.holiday.repository.EmployeeRepository;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,16 +17,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class MainView extends VerticalLayout {
     private final EmployeeRepository employeeRepository;
 
+
     private Grid<Employee> grid = new Grid<>(Employee.class); //класс где будем отображать
     //графический интерфейс
 
+    private final TextField filter = new TextField("", "Type for filter");
+    private final Button addNewBtn = new Button("add new");
+    private final HorizontalLayout toolbar = new HorizontalLayout(filter,addNewBtn);
+    private final EmployeeEditor editor;
+
 
     @Autowired
-    public MainView(EmployeeRepository employeeRepository) {
+    public MainView(EmployeeRepository employeeRepository, EmployeeEditor editor) {
         this.employeeRepository = employeeRepository;
-        add(grid);
+        this.editor = editor;
 
+        add(toolbar,grid,editor); //расположение элементов
+
+        grid.setHeight("300px");
+        grid.setColumns("id", "firstName", "lastName", "patronymic ");
+        grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
+
+        filter.setPlaceholder("Filter by last name");
+
+
+        filter.setValueChangeMode(ValueChangeMode.EAGER); //обновляет статус после каждого нажатия кнопки
+        filter.addValueChangeListener(e -> ShowEmployee(e.getValue()));
+
+
+        grid.asSingleSelect().addValueChangeListener(e -> { //выбираем строку
+            editor.editEmployee(e.getValue());
+        });
+
+
+        addNewBtn.addClickListener(e -> editor.editEmployee(new Employee()));
+
+        editor.setChangeHandler(() -> {
+            editor.setVisible(false);
+            ShowEmployee(filter.getValue());
+        });
         ShowEmployee("");
+
+
     }
 
     private void ShowEmployee(String name) { // метод для отображения базы данных
